bd06f28e02e7f7b07490c84156920a1b169e908e
mansi-dabhi <109684397+mansi-dabhi@users.noreply.github.com>
2024-02-19 21:42:12 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,17 @@
+package tnsSession;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("value of a is:"+a);
+		System.out.println("value of b is:"+b);
+		
+
+	}
+
+}
